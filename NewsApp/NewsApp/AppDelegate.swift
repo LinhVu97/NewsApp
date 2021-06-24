55b64802887ce06516be6dd166e033eeea7a64cf
LinhVN <linhvn2303@gmail.com>
2021-06-24 14:51:02 +0700

    Remove SceneDelegate

--- NewsApp/NewsApp/AppDelegate.swift
+++ NewsApp/NewsApp/AppDelegate.swift
@@ -10,25 +10,17 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let homeVC = HomeViewController()
+        self.window?.rootViewController = homeVC
+        self.window?.makeKeyAndVisible()
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication,
-                     configurationForConnecting connectingSceneSession: UISceneSession,
-                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-
     }
 
     // MARK: - Core Data stack
