d1261d97f812d57007b00d5f12f9dee76abfab49
LinhVN <linhvn2303@gmail.com>
2021-06-25 18:07:49 +0700

    Home, Setting, About, Policy Screen UI
    Update

---
+++ NewsApp/NewsApp/Helpers/Localized.swift
@@ -0,0 +1,25 @@
+//
+//  Localized.swift
+//  NewsApp
+//
+//  Created by Vũ Linh on 05/07/2021.
+//
+
+import Foundation
+import UIKit
+
+struct Localized {
+    static let history = "History".localized
+    static let changeLanguage = "Change Language".localized
+    static let policy = "Privacy Policy".localized
+    static let about = "About".localized
+    static let home = "Home".localized
+    static let search = "Search".localized
+    static let setting = "Setting".localized
+    static let business = "Business".localized
+    static let tech = "Technology".localized
+    static let sports = "Sports".localized
+    static let error = "Error".localized
+    static let cannotLoadData = "Cannot load data".localized
+    static let OK = "OK".localized
+}
